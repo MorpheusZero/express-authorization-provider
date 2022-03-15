f7c7ad5fb71bbe1c166f2f6ce45be85b8503a760
Dylan Legendre <dylanlegendre09@gmail.com>
2022-03-15 17:14:38 -0500

    adding more documentation + updating to latest version of Typescript for compile time

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+/**
+ * ######################################
+ *
+ *    Express Authorization Provider
+ *
+ *     http://www.dylanlegendre.com/
+ *
+ *  Everything gets exported at the root.
+ *
+ * ######################################
+ */
+
 export * from "./core/express-authorization-provider";
 export * from "./lib/interfaces/authorization-provider-options.interface";
